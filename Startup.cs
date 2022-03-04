ad3363817b70b324efd2dc6d0e0051ede040fc96
emipignata <emipignata@gmail.com>
2022-03-04 10:39:05 -0300

    falta correr en la consola el add migration y el update database

--- Startup.cs
+++ Startup.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Omnicanal.Database;
 using Omnicanal.Models;
 
 namespace Omnicanal
