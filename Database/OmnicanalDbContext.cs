7d06e480f9673c9673fa8165c1ed3420548ac02a
emipignata <emipignata@gmail.com>
2022-03-03 21:52:42 -0300

    instale entityFramework y debContext

---
+++ Database/OmnicanalDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Omnicanal.Models;
+
+namespace Omnicanal.Database
+{
+    public class OmnicanalDbContext : DbContext
+    {
+        public OmnicanalDbContext(DbContextOptions<OmnicanalDbContext> opciones) : base(opciones)
+        {
+        }
+
+        public DbSet<Admin> Admines { get; set; }
+        public DbSet<Asesor> Asesores { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Contacto> Contactos { get; set; }
+        public DbSet<Mensaje> Mensajes { get; set; }
+
+      //  public DbSet<Models.Enums.Canal> Canales { get; set; }
+      // porquee no estan los Roles?????????????
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Omnicanal.Models;
 
 namespace Omnicanal
 {
@@ -20,10 +22,22 @@ namespace Omnicanal
 
         public IConfiguration Configuration { get; }
 
+        public static void ConfigCookie(CookieAuthenticationOptions options)
+        {
+      //      options.LoginPath = "/Accesos/Ingresar"; // ruta relativa para login.
+      //      options.AccessDeniedPath = "/Accesos/NoAutorizado"; // ruta relativa para accesos no autorizados por falta de permisos en el rol.
+      //      options.LogoutPath = "/Accesos/Salir"; // ruta relativa para logout
+            options.ExpireTimeSpan = new System.TimeSpan(1, 0, 0); // tiempo de permanencia logueado
+            options.SlidingExpiration = true; // renuevo la cookie
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(ConfigCookie);
             services.AddControllersWithViews();
+            services.AddDbContext<OmnicanalDbContext>(options => options.UseSqlite("filename = omnicanal.db"));
+          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
